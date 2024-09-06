4c6803df892950188cd66d9a654912f38cc08d08
Esther <git@0x39b.fr>
2024-09-06 09:49:18 +0200

    Add the method to download the analysis of a PiRogue experiment

--- colander_client/domain/pirogue_experiment.py
+++ colander_client/domain/pirogue_experiment.py
@@ -2,6 +2,30 @@ from .base import BaseClientDomain
 
 
 class PirogueExperiment(BaseClientDomain):
+
+    def get_experiments(self, case=None, name=None):
+        # Inputs assertion
+        # Here we can query without case_id if needed
+
+        # Sanitize inputs
+        if case is None:
+            case = self._get_current_case()
+
+        # Query crafting
+        search_params = dict()
+        if case is not None:
+            search_params['case_id'] = case['id']
+        if name is not None:
+            search_params['name'] = name
+
+        return self._action(['pirogue_experiments', 'list'], search_params, validate=False)
+
+    def get_pirogue_experiment_analysis(self, experiment_id):
+        return self._action(['pirogue_experiments', 'analysis'], {'id': experiment_id})
+
+    def get_pirogue_experiment(self, experiment_id):
+        return self._action(['pirogue_experiments', 'read'], {'id': experiment_id})
+
     def create_pirogue_experiment(self, name=None, case=None, pcap=None, socket_trace=None, sslkeylog=None, extra_params=None):
 
         # Inputs assertion
